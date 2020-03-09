d83473c0234211b6b0826defca030e1230da9f9f
riizvy <najjmulrizvy@gmail.com>
2020-03-09 10:43:41 +0600

    finish standard rule

--- app.js
+++ app.js
@@ -9,3 +9,133 @@ GAME RULES:
 
 */
 
+ var scores, roundScore, activePlayer, gamePlaying; // dice
+
+ init();
+
+ /* scores = [0, 0];
+ roundScore = 0;
+ activePlayer = 0; */
+
+ /* dice = Math.floor(Math.random() * 6) + 1;
+ console.log(dice);
+
+ document.querySelector('#current-' + activePlayer).textContent = dice;
+ //document.querySelector('#current-' + activePlayer).innerHTML = '<i>' + dice + '</i>';
+
+ var x = document.querySelector('#score-0').textContent;
+ console.log(x)  */
+
+ /* document.querySelector('.dice').style.display = 'none';
+
+ document.getElementById('score-0').textContent = '0';
+ document.getElementById('score-1').textContent = '0';
+ document.getElementById('current-0').textContent = '0';
+ document.getElementById('current-1').textContent = '0'; */
+
+ document.querySelector('.btn-roll').addEventListener('click', function(){
+
+  if(gamePlaying){
+ 
+  // Random Number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // Display the result
+  var diceDOM = document.querySelector('.dice');
+  
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  // Update the round score IF the roll scored number was not a 1
+  if(dice !== 1){
+
+      // Add Score
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+    } else {
+      
+      // Next Player
+      nextPlayer();
+    }
+  }
+ });
+
+ document.querySelector('.btn-hold').addEventListener('click', function(){
+  
+  if (gamePlaying){
+    // Add Current Scroe to Global Score
+    scores[activePlayer] += roundScore;
+  
+    // Update Ui
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    // nextPlayer();
+    
+    // Check If Player won or not
+    if (scores[activePlayer] >= 100){
+
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+      gamePlaying = false;
+
+    } else {
+
+      // Next Player
+      nextPlayer();
+
+    }
+
+    // nextPlayer();
+      }
+ });
+
+ function nextPlayer(){
+   
+  // Next Player
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  // document.querySelector('.player-0-panel').classList.remove('active');
+  // document.querySelector('.player-1-panel').classList.add('active');
+
+  document.querySelector('.dice').style.display = 'none';
+ }
+
+//  document.querySelector('.btn-new').addEventListener('click', function() {})
+ document.querySelector('.btn-new').addEventListener('click', init)
+
+ function init(){
+  
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  gamePlaying = true;
+ 
+  document.querySelector('.dice').style.display = 'none';
+ 
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.remove('active');
+
+  document.querySelector('.player-0-panel').classList.add('active');
+
+ }
